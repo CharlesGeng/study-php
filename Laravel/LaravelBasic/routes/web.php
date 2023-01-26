4ba19d4b9e8962773bef93d007b98f1d365466cc
GENG QI <geng-q@x1studio.co.jp>
2023-01-26 18:28:18 +0900

    Laravel study

--- Laravel/LaravelBasic/routes/web.php
+++ Laravel/LaravelBasic/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $files = Post::all();
+    // ddd($files);
+    return view('posts', [
+        'posts' => $files
+    ]);
+});
+
+Route::get('posts/{id}', function ($id) {
+
+    return view('post', [
+        'post' => Post::find($id)
+    ]);
 });
